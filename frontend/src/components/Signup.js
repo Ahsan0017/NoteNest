9a6269d15ba341cff801dc4a4adeb2e72d25c1c9
Ahsan <161804838+Ahsan0017@users.noreply.github.com>
2025-08-05 14:25:20 +0530

    Updated backend host URL

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -26,7 +26,7 @@ const Signup = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/createuser", {
+      const response = await fetch("https://notenest-backend-yn27.onrender.com/api/auth/createuser", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
--- frontend/src/context/notes/NoteState.js
+++ frontend/src/context/notes/NoteState.js
@@ -2,7 +2,7 @@ import NoteContext from "./noteContext";
 import { useState, useEffect } from "react";
 
 const NoteState = (props) => {
-  const host = "http://localhost:5000";
+  const host = "https://notenest-backend-yn27.onrender.com/";
 
   const notesInitial = [];
   const [notes, setNotes] = useState(notesInitial);
