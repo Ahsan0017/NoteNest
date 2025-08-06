08a63d0eb1ba4479b5d5c744d75cec44db84f6f0
Ahsan <161804838+Ahsan0017@users.noreply.github.com>
2025-08-07 00:58:24 +0530

    Updated About page to use NoteNest

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -6,7 +6,7 @@ const About = () => {
       {/* Page Heading */}
       <div className="row justify-content-center mb-4">
         <div className="col-md-10 text-center">
-          <h2 className="fw-bold text-primary">About iNotebook</h2>
+          <h2 className="fw-bold text-primary">About NoteNest</h2>
           <p className="lead">
             iNotebook is a modern and secure note-taking app built using React for frontend and Node.js for backend. It helps you store, edit, and manage your notes easily and securely.
           </p>
